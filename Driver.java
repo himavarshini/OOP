5dc446e06ce96bde122f23f1f0202b190ea6763b
himavarshini <himayarragangu@gmail.com>
2017-01-07 15:55:33 +0530

    Driver class for testing basic set and get functions of a Fraction class

---
+++ Driver.java
@@ -0,0 +1,11 @@
+package Fraction;
+
+public class Driver {
+	public static void main(String [] args)
+	{
+		Fraction f = new Fraction(0,0);
+		f.setNumerator(2);
+		f.setDenominator(2);
+		f.print();
+	}
+}
